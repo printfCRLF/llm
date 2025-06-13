92a9c57c15a3c059bfaed0c623d714bfb3f47326
Bowen <bowen.sui@hotmail.com>
2025-06-13 23:44:31 +0200

    apikey and simple chat

--- DuelOfAgentsConsole/Program.cs
+++ DuelOfAgentsConsole/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenAI.Chat;
+using Microsoft.Extensions.Configuration;
+
+namespace DuelOfAgentsConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            string? apiKey = Environment.GetEnvironmentVariable("openai_apikey");
+
+
+            Console.WriteLine("Duel of Agents Console Application");
+
+            ChatClient client = new(model: "gpt-4o-mini", apiKey: apiKey);
+            ChatCompletion completion = await client.CompleteChatAsync("When and where was Voyado founded?");
+            Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+
+            // Keep the console window open
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
